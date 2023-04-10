84b9232a1b624ea5a99a8c21c7e69c97e92164f8
Jules PROUST <proust.jules@gmail.com>
2023-04-10 23:26:28 +0200

    Passage au port 80

--- src/main/java/projet/jee/controller/HelloController.java
+++ src/main/java/projet/jee/controller/HelloController.java
@@ -1,11 +1,8 @@
 package projet.jee.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HelloController {
