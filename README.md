d1814d92fd4270ef8b4f0181dd26e26266cac725
Echelon <proust.jules@gmail.com>
2023-04-20 03:30:10 +0200

    Rapport

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Feur
 
 Projet en Applications d'entreprise en Java Spring Boot pour la fin d'année 2022-2023 en L3 Informatique à l'Université de Tours.\
-Rapport final disponible : fichier rendu.pdf
+Rapport final disponible : dossier rendu
 
-Etudiants:
+SQL:
+---
+Ce projet fonctionne avec une base de données SQL externe.\
+Créez une base de données et exécutez-y *rendu/dump.sql* pour créer les différentes tables, puis *rendu/remplissage.sql* si vous souhaitez la remplir.\
+Renseignez les informations de cette bdd dans *application.yaml* dans la partie *datasource*.
+
+Étudiants:
 ---
 - Patry Simon
 - Proust Jules
---
+++ rendu/dump.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: ProjetJEE
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activity`
+--
+
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activity` (
+  `activity_id` bigint NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `description` text,
+  `mail` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `notation_average` float DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `ville` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `activity_seq`
+--
+
+DROP TABLE IF EXISTS `activity_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activity_seq` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `app_user`
+--
+
+DROP TABLE IF EXISTS `app_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `app_user` (
+  `user_id` bigint NOT NULL,
+  `admin` bit(1) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UK_3k4cplvh82srueuttfkwnylq0` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `app_user_seq`
+--
+
+DROP TABLE IF EXISTS `app_user_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `app_user_seq` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subscription`
+--
+
+DROP TABLE IF EXISTS `subscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subscription` (
+  `note` int DEFAULT NULL,
+  `user_user_id` bigint NOT NULL,
+  `activity_activity_id` bigint NOT NULL,
+  PRIMARY KEY (`activity_activity_id`,`user_user_id`),
+  KEY `FKiojbxojf74jtd344epc817epi` (`user_user_id`),
+  CONSTRAINT `FK3dg4awk4k6egrjkaj0ws6q5cv` FOREIGN KEY (`activity_activity_id`) REFERENCES `activity` (`activity_id`),
+  CONSTRAINT `FKiojbxojf74jtd344epc817epi` FOREIGN KEY (`user_user_id`) REFERENCES `app_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-15 21:27:56
